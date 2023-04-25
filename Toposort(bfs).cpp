5da1999490c61d86daa03a2b7698d511a3e7c130
abrknine <abhishekbahukhandi2020@gmail.com>
2023-04-26 01:55:44 +0530

    toposort(bfs)

---
+++ Toposort(bfs).cpp
@@ -0,0 +1,53 @@
+
+#include<unordered_map>
+#include<vector>
+#include<queue>
+#include<list>
+
+
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e) {
+    unordered_map<int, list<int>> adj;
+   for( int i=0; i<e; i++){
+       int u=edges[i][0];
+       int v=edges[i][1];
+       adj[u].push_back(v);
+   }
+
+  //making indegree vector 
+  vector<int> indegree(v);
+  for(auto i:adj){
+      for(auto j:i.second){
+          indegree[j]++;
+      }
+  }
+  queue<int> q;
+  for( int i=0; i<indegree.size(); i++){
+      if(indegree[i]==0){
+          q.push(i);
+      }
+  }
+  vector<int> ans;
+  while(!q.empty()){
+
+     int front = q.front();
+     q.pop();
+          ans.push_back(front);
+    for(auto neighbour:adj[front]){
+      indegree[neighbour]--;
+      if(indegree[neighbour]==0){
+          q.push(neighbour);
+
+      }
+
+
+    }
+
+
+
+  }
+
+ 
+     return ans;
+
+    
+}
