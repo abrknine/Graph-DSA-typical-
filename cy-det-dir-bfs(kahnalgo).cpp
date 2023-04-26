ddcd8018398e4895ed15d6e2012915d369f3b958
abrknine <abhishekbahukhandi2020@gmail.com>
2023-04-27 01:24:50 +0530

    cy-det-dir-bfs

---
+++ cy-det-dir-bfs(kahnalgo).cpp
@@ -0,0 +1,52 @@
+#include<unordered_map>
+#include <list>
+#include <queue>
+
+
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges) {
+  unordered_map<int, list<int>> adj;
+  for(int i=0; i<edges.size(); i++){
+     int u=edges[i].first-1;
+     int v=edges[i].second-1;
+     adj[u].push_back(v);
+  }
+
+  vector<int> indegree(n);
+  for(auto i:adj){
+    for(auto j :i.second){
+      indegree[j]++;
+    }
+  }
+
+  queue<int> q;
+  for( int i=0; i<n; i++){
+  if(indegree[i]==0){
+    q.push(i);
+  }
+  }
+  int c=0;
+  while(!q.empty()){
+   int front=q.front();
+   q.pop();
+ c++;
+   
+
+   for(auto neighbour:adj[front]){
+      indegree[neighbour]--;
+      if(indegree[neighbour]==0){
+           q.push(neighbour);
+
+      }
+   }
+   
+ 
+
+  }
+  if(c==n){
+    return false;
+  }
+  else{
+    return true;
+  }
+
+}
